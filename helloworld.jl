f7287f8ff14629b082d4042e2086cc1c9d773be0
fernal73 <linus.fernandes@gmail.com>
2021-05-02 11:15:22 +0530

    Hello, world

---
+++ helloworld.jl
@@ -0,0 +1,18 @@
+#!/usr/bin/env julia
+println("There is more than one way to say hello:")
+# This is an array consisting of three strings
+helloArray = ["Hello","G’day","Shalom", "Aloha"]
+for i in 1:4
+println("\t", helloArray[i], ", World!")
+end
+println("\nThese squares are just perfect:")
+# This construct is called a ‘comprehension’ (or ’list comprehension’)
+squares = [i^2 for i in 0:10]
+# You can loop on elements of arrays without having to use indexing
+for s in squares
+print(" ",s)
+end
+println()
+# The last line of every code snippet is also evaluated as output (in addition to
+# any figures and printing output generated previously).
+println(sqrt.(squares))
