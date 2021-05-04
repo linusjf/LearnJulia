fc55f851d2c2cd20559b80074d60309b47d81fe8
fernal73 <linus.fernandes@gmail.com>
2021-05-04 22:13:31 +0530

    Add more julia samples

---
+++ Basic/pseudo.jl
@@ -0,0 +1,8 @@
+#!/usr/bin/env julia
+using Random
+Random.seed!(1974)
+println("Seed 1974: ",rand(),"\t", rand(), "\t", rand())
+Random.seed!(1975)
+println("Seed 1975: ",rand(),"\t", rand(), "\t", rand())
+Random.seed!(1974)
+println("Seed 1974: ",rand(),"\t", rand(), "\t", rand())
